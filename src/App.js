bce2770dccb7d32acd505409849367680009b037
shafin_shant0 <shafinshanto9@gmail.com>
2022-07-18 12:15:49 +0600

    project setup completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
-import './App.css';
+
+import 'antd/dist/antd.css';
+import { Button } from 'antd';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import HomePage from './pages/HomePage';
+import Items from './pages/Items';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/homepage' element={ <HomePage/>} />
+          <Route path='/item' element={ <Items/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
