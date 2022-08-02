03df26fe613e9ddd43fcc21b95bc72efd7336a8e
shafin_shant0 <shafinshanto9@gmail.com>
2022-08-02 16:48:30 +0600

    cart calc func charge bill func

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,10 +1,10 @@
-import { Button, Modal, Table } from 'antd'
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import DefaultLayout from '../components/DefaultLayout'
 import {DeleteOutlined, PlusCircleOutlined,MinusCircleOutlined} from '@ant-design/icons'
 import { useState } from 'react'
 import '../resources/cartPage.css'
+import { Button, Form, Input, message, Modal, Select, Table } from 'antd'
 
 const CartPage = () => {
 
@@ -74,7 +74,19 @@ const CartPage = () => {
         });
         setSubTotal(tempTotal)
         
-    },[cartItems])
+    }, [cartItems])
+
+    
+    const onFinish = (values) => {
+        const reqObj = {
+            ...values,
+            subTotal,
+            TAX: Number(((subTotal / 100) * 10).toFixed(2)),
+            TotalAmount: Number((subTotal+ Number((subTotal / 100)*10)).toFixed(2)),
+            userId: JSON.parse(localStorage.getItem('pos-user'))._id
+        }
+        console.log(reqObj);
+    }
 
 
   return (
@@ -88,7 +100,40 @@ const CartPage = () => {
               </div>
               <Button onClick={()=>{setBillChargeModal(true)}}>Charge Bill</Button>
           </div>
-          <Modal title='Charge Bill' visible={billChargeModal}></Modal>
+          <Modal title='Charge Bill' visible={billChargeModal} footer={false}
+            onCancel={() => { setBillChargeModal(false) }}>
+            <Form  layout='vertical' onFinish={onFinish}>
+
+            <Form.Item name='customerName' label='Customer Name'>
+                <Input   />
+            </Form.Item>
+            <Form.Item name='customerPhoneNumber' label='Phone Number'>
+                <Input />
+            </Form.Item>
+            <Form.Item name='paymentMode' label='Payment Mode'>
+                <Select>
+                <Select.Option value='cash' >Cash</Select.Option>
+                <Select.Option value='card' >Card</Select.Option>
+                <Select.Option value='mobileBanking' >Mobile Banking</Select.Option>
+                </Select>
+            </Form.Item>
+                  
+            <div className='charge-bill-amount'>
+                <h5>SUBTOTAL: <b>$ {subTotal}</b></h5>
+                <h5>TAX: <b>$ { ((subTotal / 100)*10).toFixed(2)}</b></h5>     
+                <hr />
+                <h2>GRAND TOTAL: <b>$ { (subTotal+ (subTotal / 100)*10).toFixed(2) }</b></h2>      
+            </div>
+
+          <div className='d-flex justify-content-end'>
+             <button
+              className='px-4 py-2'
+              type='submit'
+              style={{ backgroundColor: '#9E6051', color: 'white', border: 'none', textAlign: 'center', borderRadius: '5px' }}>
+              GENERATE BILL</button>
+          </div>
+        </Form>
+          </Modal>
     </DefaultLayout>
   )
 }
